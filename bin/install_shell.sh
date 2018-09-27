42999e48ca0c2150609f9d2b9d303739d44c8f83
Jinhong Jung <jinhongjung@snu.ac.kr>
2018-09-27 22:51:02 +0900

    add setup for dircolors

--- bin/install_shell.sh
+++ bin/install_shell.sh
@@ -8,3 +8,12 @@ echo "[shell] end shell installation"
 echo "[ssh] start setting up ssh config"
 cp ssh/config ~/.ssh
 echo "[ssh] end setting up ssh config"
+
+echo "[dircolors] start installing dircolors"
+if [ ! -d ~/.dircolors ]; then
+    mkdir ~/.dircolors
+fi
+
+git clone https://github.com/gibbling666/dircolors.git ~/.dircolors
+
+echo "[dircolors] end installing dircolors"
